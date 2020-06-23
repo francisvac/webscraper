9327afa6fd5148b39c860534bb5aa53d8664f971
francisvac <francisvac+git@gmail.com>
2020-06-22 21:34:56 -0700

    updates

--- dev_start.sh
+++ dev_start.sh
@@ -1,7 +1,7 @@
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker run -it -d \
-     -v $HOME/dev/webscraper:$HOME \
+     -v $HOME/dev/webscraper:/dev/webscraper \
      --name docker-webscraper  \
      --gpus all \
      -p 8888:8888 \
