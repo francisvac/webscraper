31e5815971749810db6ff5bfea427f110f89ac0f
francisvac <francisvac+git@gmail.com>
2020-06-25 10:02:23 -0700

    fix loc

--- dev_start.sh
+++ dev_start.sh
@@ -1,7 +1,7 @@
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker run -it -d \
-     -v $HOME/dev/linkedin_scraper:/dev/webscraper \
+     -v $HOME/dev/webscraper:/dev/webscraper \
      --name docker-linkedin \
      --gpus all \
      -p 8888:8888 \
